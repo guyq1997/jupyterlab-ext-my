489b0c2c07b314ef7353d795065ba7c1c40b4395
guyq1997 <gyq673516081@gmail.com>
2024-06-20 10:40:09 +0200

    1

---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+import { JupyterFrontEndPlugin } from '@jupyterlab/application';
+/**
+ * Initialization data for the jupyterlab_apod extension.
+ */
+declare const plugin: JupyterFrontEndPlugin<void>;
+export default plugin;
